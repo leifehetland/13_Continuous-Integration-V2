71bb3e4c5e670a92130109658797b122b01c87eb
leifehetland <lehetland1@gmail.com>
2025-03-05 21:13:47 -0500

    updated server to include process.cwd()

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-
+import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
 await db();
 
-const PORT = parseInt(process.env.PORT || '3001', 10);
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,10 +13,10 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/dist'));
+    app.use(express.static(path.join(process.cwd(), '../client/dist')));
 
   app.get('*', (_req, res) => {
-    res.sendFile('../client/dist/index.html');
+    res.sendFile(path.join(process.cwd(), '../client/dist/index.html'));
   });
 }
 
